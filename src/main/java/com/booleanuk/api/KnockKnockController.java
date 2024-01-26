c5073fed5659ed64707a2af573115512b86d98ef
Alexander Niklasson <alexander.niklasson@live.com>
2024-01-26 11:37:44 +0100

    Refactored code

--- src/main/java/com/booleanuk/api/KnockKnockController.java
+++ src/main/java/com/booleanuk/api/KnockKnockController.java
@@ -2,6 +2,7 @@ package com.booleanuk.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,49 +26,25 @@ public class KnockKnockController {
         this.rand = new Random();
         this.index = rand.nextInt(jokes.size());
     }
-
-    @GetMapping("/line1")
-    public String getFirstLine(){
-        this.index = rand.nextInt(jokes.size());
-        return this.jokes.get(index).get(0);
-    }
-    @GetMapping("/line2")
-    public String getSecondLine(){
-        return this.jokes.get(index).get(0) +
-                "\n\n" + this.jokes.get(index).get(1);
-    }
-    @GetMapping("/line3")
-    public String getThirdLine(){
-        return this.jokes.get(index).get(0) +
-                "\n\n" + this.jokes.get(index).get(1) +
-                "\n\n" + this.jokes.get(index).get(2);
-    }
-    @GetMapping("/line4")
-    public String getFourthLine(){
-        return this.jokes.get(index).get(0) +
-                "\n\n" + this.jokes.get(index).get(1) +
-                "\n\n" + this.jokes.get(index).get(2) +
-                "\n\n" + this.jokes.get(index).get(3);
-    }
-    @GetMapping("/line5")
-    public String getFifthLine(){
-        return this.jokes.get(index).get(0) +
-                "\n\n" + this.jokes.get(index).get(1) +
-                "\n\n" + this.jokes.get(index).get(2) +
-                "\n\n" + this.jokes.get(index).get(3) +
-                "\n\n" + this.jokes.get(index).get(4);
-    }
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Path not found")
-    public static class NotFoundException extends RuntimeException {
-        public NotFoundException(String message) {
-            super(message);
+    @GetMapping("line/{lineNumber}")
+    public String getLine(@PathVariable int lineNumber){
+        if(lineNumber > 5){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found");
         }
+        else if(lineNumber == 1){
+            this.index = rand.nextInt(jokes.size());
+            return this.jokes.get(index).get(0);
+        }
+        else{
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < lineNumber; i++){
+                sb.append(jokes.get(index).get(i)).append("\n\n");
+            }
+            return sb.toString();
+        }
+
     }
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundException(NotFoundException ex) {
-        return "Page does not exist";
-    }
+
 
 
 }
--- src/test/java/com/booleanuk/api/KnockKnockControllerTest.java
+++ src/test/java/com/booleanuk/api/KnockKnockControllerTest.java
@@ -8,13 +8,13 @@ public class KnockKnockControllerTest {
     @Test
     public void testGettingRandomJokeLineByLine(){
         KnockKnockController knockKnockController = new KnockKnockController(100);
-        knockKnockController.getFirstLine();
+        knockKnockController.getLine(1);
         Assertions.assertEquals("Knock, Knock.\n\nWho's There?\n\nLettuce." +
-                "\n\nLettuce who?\n\nLettuce in, it's freezing out here", knockKnockController.getFifthLine());
+                "\n\nLettuce who?\n\nLettuce in, it's freezing out here\n\n", knockKnockController.getLine(5));
 
-        knockKnockController.getFirstLine();
+        knockKnockController.getLine(1);
         Assertions.assertEquals("Knock, Knock.\n\nWho's There?\n\nJustin." +
-                "\n\nJustin who?\n\nJustin time for dinner!", knockKnockController.getFifthLine());
+                "\n\nJustin who?\n\nJustin time for dinner!\n\n", knockKnockController.getLine(5));
 
     }
 }
